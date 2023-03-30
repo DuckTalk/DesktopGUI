dcf2c822ea712acffeb2c809971d4dcd0495dabb
shala190035 <55692386+shala190035@users.noreply.github.com>
2023-03-30 12:50:48 +0200

    update

--- landing_page/src/preload.js
+++ landing_page/src/preload.js
@@ -3,17 +3,19 @@
 const { ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
-  const sendButton = document.getElementById('send-button');
+  const messageForm = document.querySelector('.message-form');
 
-  sendButton.addEventListener('click', function() {
-      // Make an HTTP request to the API endpoint
-      const apiUrl = 'http://ableytner.ddns.net:2006/api/user';
-      fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-          // Send the data back to the main process
-          ipcRenderer.send('api-response', data);
-      })
-      .catch(error => console.error(error));
+  messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Make an HTTP request to the API endpoint
+    const apiUrl = 'http://ableytner.ddns.net:2006/api/user';
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Send the data back to the main process
+      ipcRenderer.send('api-response', data);
+    })
+    .catch(error => console.error(error));
   });
-})
+});
