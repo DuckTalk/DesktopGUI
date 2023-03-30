b4b071ba986549e797b0336a22b95459f19cce6d
lukasheiling <55692511+lukasheiling@users.noreply.github.com>
2023-03-30 12:55:14 +0200

    POST_funktioniert

--- login/src/preload.js
+++ login/src/preload.js
@@ -3,17 +3,28 @@
 const { ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
-  const sendButton = document.getElementById('send-button');
+const sendButton = document.getElementById('send-button');
 
-  sendButton.addEventListener('click', function() {
-      // Make an HTTP request to the API endpoint
-      const apiUrl = 'http://ableytner.ddns.net:2006/api/user';
-      fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-          // Send the data back to the main process
-          ipcRenderer.send('api-response', data);
-      })
-      .catch(error => console.error(error));
-  });
+sendButton.addEventListener('click', function() {
+// Make an HTTP request to the API endpoint
+const apiUrl = 'http://ableytner.ddns.net:2006/api/user';
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+fetch(apiUrl, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+      email: email,
+      password: password
+  })
+})
+.then(response => response.json())
+.then(data => {
+  // Send the data back to the main process
+  ipcRenderer.send('api-response', data);
+})
+.catch(error => console.error(error));
+});
 })
