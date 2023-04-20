190cd3e1501d56b66676ec71ab32bc0274a621ed
lukasheiling <55692511+lukasheiling@users.noreply.github.com>
2023-04-20 11:26:44 +0200

    GET_funktioniert

--- login/src/preload.js
+++ login/src/preload.js
@@ -1,34 +1,58 @@
-import axios from 'axios';
+// preload.js
+const loginButton = document.getElementById('login-button');
+const { ipcRenderer } = require('electron');
 
-export default {
-  data() {
-    return {
-      type: 'user',
-      auth: JSON.stringify({
-        type: "token",
-        token: ""
-      }),
-      data: JSON.stringify({
-        user_id: 1
-      })
-    };
+window.addEventListener('DOMContentLoaded', () => {
+const sendButton = document.getElementById('login-button');
+
+sendButton.addEventListener('click', function() {
+// Make an HTTP request to the API endpoint
+const apiUrl = 'http://ableytner.ddns.net:2006/api/user/test2';
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+/*fetch(apiUrl, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json'
   },
-  methods: {
-    handleSubmit() {
-      // Call the get_responds method when the form is submitted
-      this.get_responds();
+  body: JSON.stringify({
+      type: 'user',
+      data: {
+        username: 'testuser',
+        email: 'test@gmail.com',
+        pw_hash: 'empty1',
+        salt: '1h2jk2n'
+      }
+  })
+})
 
-      // Do something with email and password values
-    },
-    get_responds() {
-      axios.get('/api/home')
-        .then((response) => {
-          console.log("Axios Response: ", response);
-          console.log("Hallo");
-        })
-        .catch(function (error) {
-          console.error("Axios Error", error);
-        });
-    }
-  }
-};
+.then(response => response.json())
+.then(data => console.log('api-response', data))
+.catch(error => console.error(error));
+*/
+fetch(apiUrl, {
+  method: 'GET',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  json: JSON.stringify({
+      type: 'user',
+      auth: {
+        type: 'token',
+        token: ''
+      },
+      data: {
+        user_id: 1
+      }
+
+  })
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data); // log the response data to the console
+  // Send the data back to the main process
+  ipcRenderer.send('api-response', data);
+})
+.catch(error => console.error(error));
+});
+})
